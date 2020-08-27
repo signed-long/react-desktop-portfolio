a10e7929ddeba8d7a4d5e248a659a0739f09c4c9
MichaeLong <mrl@ualberta.ca>
2020-08-26 21:20:19 -0600

    Updates

---
+++ precache-manifest.4abd1ed0043c9583304ef20adc181ec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f600e4bd644cb89ede4899badf08bb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e7fd3a2707b6873f67c",
+    "url": "/static/js/2.2ca55138.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2ca55138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec9fce554f8515e0c43",
+    "url": "/static/js/main.bcdfbf1d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
